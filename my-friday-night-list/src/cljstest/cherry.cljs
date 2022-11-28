c1e6d0d889b2f2bb42e06f35b9eee31645f7c65b
Anthial <faildude@live.se>
2022-11-28 14:05:26 +0100

    delete old folder

--- my-friday-night-list/src/cljstest/cherry.cljs
+++
@@ -1,8 +0,0 @@
-(ns cljstest.cherry)
-
-
-
-(defn Cherryworks-button [props] 
-  (let [x (get (js->clj props) "x")]
-  #jsx [:button {:onClick (fn [] (js/alert (str "Hello from CLJS! You've fed me: " (js->clj props) "! I see: " x "!")))} "Click me!"]))
-(def default Cherryworks-button)
