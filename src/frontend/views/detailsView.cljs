b2f7880309bbfaa5b70cad9a16f919fe8efe11e6
Anthial <faildude@live.se>
2022-12-02 16:59:02 +0100

    update details view to follow IMDB API

--- src/frontend/views/detailsView.cljs
+++ src/frontend/views/detailsView.cljs
@@ -1,22 +1,33 @@
 (ns frontend.views.detailsView)
 
+(defn generate-view [episode]
+  (let [id (:id episode)
+        episode-number (:episodeNumber episode)
+        title (:title episode)
+        image (:image episode)
+        release (:released episode)
+        description (:plot episode)]
+    #jsx [:div {:key id :className "flex container flex-col items-center text-center w-full md:w-80 h-[600px] mt-2 bg-[#006466] p-8 lg:m-5 rounded-lg"}
+          [:div [:img {:src image :className "flex w-80"}]]
+          [:div {:className "flex flex-col items-center text-center mt-2"} [:div {:className "w-40 font-bold text-2xl lg:text-2xl lg:w-64"} "Episode " episode-number ": " title]]
+          [:div {:className "flex flex-col items-center mt-4"} [:span {:className "w-64 h-40 text-elipsis overflow-hidden ..."} description]]
+          [:div {:className "flex justify-center text-center mt-3 relative w-full h-32"}
+           ;[:div {:className "flex flex-col justify-center"} Remember for Seasons interface,
+           [:div {:className "w-30 font-bold text-md absolute inset-x-0 bottom-0"} "Episode Released: " release]]]))
+
+
 (defn details-view [props]
   (let [props (js->clj props)
-        image (:img props)
         title (:title props)
-        show-start (:show-start props)
-        show-end (:show-end props)
-        description (:description props)]
+        year (:year props)
+        episodes (:episodes props)]
     #jsx [:div {:className "flex justify-center h-screen w-full"}
-          [:div {:className "container justify-center w-96 bg-[#006466] p-8 m-10 rounded-lg"}
-           [:div [:img {:src image :className "flex w-80 "}]]
-           [:div {:className "flex flex-col items-center text-center mt-2"} [:div {:className "w-40 font-bold text-2xl lg:text-4xl lg:w-64"} title]]
-           [:div {:className "flex flex-col items-center mt-4"} [:span {:className "w-64"} description]]
-           [:div {:className "flex justify-center text-center mt-3"}
-           ;[:div {:className "flex flex-col justify-center"} Remember for Seasons interface,
-            [:div {:className "w-30 font-bold text-md"} "Show started in: "show-start]
-            [:div {:className "w-30 font-bold text-md"} "Show ended in: "show-end]]]
-          ]))
+          [:div {:className "container justify-center w-full"}
+           
+           [:div {:className "flex flex-col items-center"}[:div {:className "flex flex-row items-center text-center mt-2"} [:div {:className "w-40 font-bold text-2xl lg:text-4xl lg:w-64"} title "-" year]
+            [:button {:className "bg-[#4D194D] font-bold"}"Trivia"]]]
+           [:div {:className "flex flex-row flex-wrap container w-128 justify-center items-center text-center mt-2"}
+           (map #(generate-view %) episodes)]]]))
 
 
 
--- src/frontend/views/detailsView.jsx
+++ src/frontend/views/detailsView.jsx
@@ -1,16 +1,26 @@
-import { js__GT_clj, keyword } from 'cherry-cljs/lib/cljs_core.js'
+import { keyword, js__GT_clj, map } from 'cherry-cljs/lib/cljs_core.js'
+var generate_view = function (episode) {
+let id1 = keyword("id").call(null, episode);
+let episode_number2 = keyword("episodeNumber").call(null, episode);
+let title3 = keyword("title").call(null, episode);
+let image4 = keyword("image").call(null, episode);
+let release5 = keyword("released").call(null, episode);
+let description6 = keyword("plot").call(null, episode);
+return <div key={id1} className="flex container flex-col items-center text-center w-full md:w-80 h-[600px] mt-2 bg-[#006466] p-8 lg:m-5 rounded-lg"><div><img src={image4} className="flex w-80"></img></div> <div className="flex flex-col items-center text-center mt-2"><div className="w-40 font-bold text-2xl lg:text-2xl lg:w-64">Episode  {episode_number2} :  {title3}</div></div> <div className="flex flex-col items-center mt-4"><span className="w-64 h-40 text-elipsis overflow-hidden ...">{description6}</span></div> <div className="flex justify-center text-center mt-3 relative w-full h-32"><div className="w-30 font-bold text-md absolute inset-x-0 bottom-0">Episode Released:  {release5}</div></div></div>;
+}
+;
 var details_view = function (props) {
-let props1 = js__GT_clj.call(null, props);
-let image2 = keyword("img").call(null, props1);
-let title3 = keyword("title").call(null, props1);
-let show_start4 = keyword("show-start").call(null, props1);
-let show_end5 = keyword("show-end").call(null, props1);
-let description6 = keyword("description").call(null, props1);
-return <div className="flex justify-center h-screen w-full"><div className="container justify-center w-96 bg-[#006466] p-8 m-10 rounded-lg"><div><img src={image2} className="flex w-80 "></img></div> <div className="flex flex-col items-center text-center mt-2"><div className="w-40 font-bold text-2xl lg:text-4xl lg:w-64">{title3}</div></div> <div className="flex flex-col items-center mt-4"><span className="w-64">{description6}</span></div> <div className="flex justify-center text-center mt-3"><div className="w-30 font-bold text-md">Show started in:  {show_start4}</div> <div className="w-30 font-bold text-md">Show ended in:  {show_end5}</div></div></div></div>;
+let props7 = js__GT_clj.call(null, props);
+let title8 = keyword("title").call(null, props7);
+let year9 = keyword("year").call(null, props7);
+let episodes10 = keyword("episodes").call(null, props7);
+return <div className="flex justify-center h-screen w-full"><div className="container justify-center w-full"><div className="flex flex-col items-center"><div className="flex flex-row items-center text-center mt-2"><div className="w-40 font-bold text-2xl lg:text-4xl lg:w-64">{title8} - {year9}</div> <button className="bg-[#4D194D] font-bold">Trivia</button></div></div> <div className="flex flex-row flex-wrap container w-128 justify-center items-center text-center mt-2">{map.call(null, function (_PERCENT_1) {
+return generate_view.call(null, _PERCENT_1);
+}, episodes10)}</div></div></div>;
 }
 ;
 var default$ = details_view
 ;
 
-export { details_view }
+export { generate_view, details_view }
 export default default$
