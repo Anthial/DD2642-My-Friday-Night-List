736b3dfafe47f312b1e1158a0bfc05e3049dfb86
Anthial <faildude@live.se>
2022-12-02 18:46:33 +0100

    add init mobile view header

--- src/frontend/header.tsx
+++ src/frontend/header.tsx
@@ -20,14 +20,26 @@ export default function Header(){
   return (
     <div>
       <div className="px-5 py-7 flex justify-evenly items-center bg-[#312244]">
-        <h1 className="font-bold text-2xl py-2">My Friday Night List</h1>
-        <div className="flex">
-          <div className="flex w-72">
-            <input className="outline-none w-72 rounded-lg bg-[#212F45] pl-4" type="text" placeholder="Search..." />
+        <div className="hidden lg:flex">
+          <h1 className="font-bold text-2xl py-2 mr-20">My Friday Night List</h1>
+          <div className="flex">
+            <div className="flex w-72">
+              <input className="outline-none w-72 rounded-lg bg-[#212F45] pl-4" type="text" placeholder="Search..." />
+            </div>
+            <ul className="flex space-x-12">
+              {buttonMap.map(HeaderButtonsCB)}
+            </ul>
           </div>
-          <ul className="flex space-x-12">
-            {buttonMap.map(HeaderButtonsCB)}
-          </ul>
+        </div>
+        
+        <div className="flex px-5 justify-start items-center lg:hidden">
+          <h1 className="font-bold text-xl mr-10">M.F.N.L</h1>
+          <div className="flex ml-2 w-50 h-12">
+              <input className="outline-none w-50 rounded-lg bg-[#212F45] pl-4" type="text" placeholder="Search..." />
+          </div>
+          <button className="bg-[#312244]"><img src="https://cpb-us-w2.wpmucdn.com/sites.widener.edu/dist/7/57/files/2019/03/Hamburger_icon_svg.png" 
+              className="w-10 h-10 outline-white"></img>
+          </button>
         </div>
       </div>
     </div>);
