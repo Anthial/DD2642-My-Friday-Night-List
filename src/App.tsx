a3ade5a2f5a3ac236620f1341bd11f8032bc8ea4
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2022-12-03 02:03:52 +0100

    add PersonalList for testing how it looks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import CherryworksButton from './cljstest/cherry.jsx'
-import Header from "./frontend/header"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import CherryworksButton from "./cljstest/cherry.jsx";
+import Header from "./frontend/header";
+import PersonalList from "./frontend/presenters/personalListPresenter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <div>
       <Header></Header>
-      <CherryworksButton x={count}></CherryworksButton>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <PersonalList></PersonalList>
+      {/* <CherryworksButton x={count}></CherryworksButton>
+      <button onClick={() => setCount(count + 1)}>{count}</button> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
