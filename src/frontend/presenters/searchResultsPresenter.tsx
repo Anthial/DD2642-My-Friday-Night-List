8ce73677663b146ec5ce15ff76190c1187d2edf3
Hannes Mann <hannesmann2000@gmail.com>
2022-12-06 19:50:16 +0100

    Improve search results design

--- src/frontend/presenters/searchResultsPresenter.tsx
+++ src/frontend/presenters/searchResultsPresenter.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { selectedTitleAtom } from "../../atoms";
 import { searchImdb } from "../../backend/api/imdb/functions";
 import { Title } from "../../backend/model/title";
 import SearchResultsView from "../views/searchResultsView";
@@ -7,9 +8,11 @@ import SearchResultsView from "../views/searchResultsView";
 export default function SearchResults() {
 	const [titles, setTitles] = useState([] as Title[]);
 
+	/* TODO: Replace with real API call */
 	useEffect(() => {
 		setTimeout(() => { searchImdb("", true).then((t) => {setTitles(t)}) }, 500);
 	}, []);
 
-	return <SearchResultsView loading={titles.length == 0} titles={titles}></SearchResultsView>;
+	const setSelectedTitle = useSetRecoilState(selectedTitleAtom);
+	return <SearchResultsView loading={titles.length == 0} titles={titles} onSelectTitle={(t: Title) => setSelectedTitle(t)}></SearchResultsView>;
 }
