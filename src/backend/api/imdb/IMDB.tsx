6c070588b2ad75bc2984a71999dd05de50805395
Hannes Mann <hannesmann2000@gmail.com>
2022-12-19 00:51:47 +0100

    Let API return reason for error

--- src/backend/api/imdb/IMDB.tsx
+++ src/backend/api/imdb/IMDB.tsx
@@ -1,19 +1,42 @@
 import {IMDB_API_KEY} from "./apiConfig"
 
+export enum ApiErrorReason {
+    Unknown,
+    LimitReached
+}
+
+class ApiError extends Error {
+    reason: ApiErrorReason = ApiErrorReason.Unknown;
+
+    constructor(message: string, reason: ApiErrorReason) {
+        super(message);
+        this.reason = reason;
+    }
+}
 
 function handleResponse(response: any){
+    let errorReason = ApiErrorReason.Unknown;
+
     if (response.status !== 200){
-        throw new Error("Could not access data, status: " + response.status);
+        /* Too Many Requests */
+        if(response.status === 429) {
+            errorReason = ApiErrorReason.LimitReached;
+        }
+
+        throw new ApiError("Could not access data, status: " + response.status, errorReason);
     }
 
     const responseObj = response.json();
     
     if (responseObj && responseObj.errorMessage) {
-        throw new Error("Could not access data, error message: " + responseObj.errorMessage);
+        if(responseObj.errorMessage.includes("Maximum usage")) {
+            errorReason = ApiErrorReason.LimitReached;
+        }
+
+        throw new ApiError("Could not access data, error message: " + responseObj.errorMessage, errorReason);
     }
 
     return responseObj;
-
 }
 
 function fetchSearchResults(query: string){
