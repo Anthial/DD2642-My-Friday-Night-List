62815795ea90511ba3dba9b9d021c8ce3f9add95
Hannes Mann <hannesmann2000@gmail.com>
2022-12-19 15:58:45 +0100

    Show back button if spinner takes too long

--- src/frontend/views/spinnerView.tsx
+++ src/frontend/views/spinnerView.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Spinner() {
+  const [showBackButton, setShowBackButton] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setShowBackButton(true), 7000);
+    return () => clearTimeout(timeout);
+  })
+
   return (
     <div className="w-full grow flex flex-col justify-center items-center">
-      <button className="self-center mb-3 bg-[#4D194D] hover:bg-[#251a33] font-bold" onClick={() => navigate(-1)}>BACK</button>
       <img src="/spinner.svg" alt="Loading" width="128" height="128" />
+      {showBackButton && <button className="self-center mb-3 bg-[#4D194D] hover:bg-[#251a33] font-bold" onClick={() => navigate(-1)}>Back</button>}
     </div>
   );
 }
