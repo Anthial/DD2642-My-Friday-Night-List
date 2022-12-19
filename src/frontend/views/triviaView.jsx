a278d9610095f2f270d21ea914a438d881641907
Anthial <faildude@live.se>
2022-12-19 15:57:49 +0100

    update trivia view

--- src/frontend/views/triviaView.jsx
+++ src/frontend/views/triviaView.jsx
@@ -12,9 +12,9 @@ let year21 = props["year"];
 let items22 = props["items"];
 let src23 = props["image"];
 let navigate24 = useNavigate.call(null);
-return <div className="flex justify-center w-full"><div className="container justify-center w-full lg:w-[50%] h-full bg-[#006466] p-8 px-4 lg:px-20 textm-2 lg:m-5 rounded-lg overscroll-auto hover:overscroll-contain text-left overflow-y-auto"><div className="flex flex-col items-center"><div className="flex flex-row items-center text-center mt-2"><button className="mr-4 bg-[#4D194D] hover:bg-[#251a33] font-bold" onClick={function () {
+return <div className="flex justify-center w-full"><div className="container justify-center w-full lg:w-[50%] h-full bg-[#006466] p-8 px-4 lg:px-20 textm-2 lg:m-5 rounded-lg overscroll-auto hover:overscroll-contain text-left overflow-y-auto"><div className="flex flex-col items-center"><div className="flex flex-row items-center text-center mt-2"><div className="w-40 font-bold text-2xl lg:text-4xl lg:w-64 ">{title20} - {year21} Trivia</div></div> <img src={src23} className="w-64 "></img> <button className="mr-4 bg-[#4D194D] hover:bg-[#251a33] self-start font-bold" onClick={function () {
 return navigate24.call(null, -1);
-}}>Back</button> <div className="w-40 font-bold text-2xl lg:text-4xl lg:w-64 ">{title20} - {year21} Trivia</div></div> <img src={src23} className="w-64 "></img></div> {map.call(null, function (_PERCENT_1) {
+}}>Back</button></div> {map.call(null, function (_PERCENT_1) {
 return generate_qa_view.call(null, _PERCENT_1);
 }, items22)}</div></div>;
 }
