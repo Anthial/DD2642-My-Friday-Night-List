b6b8ff3d2924b01d395b4702d49f368923478824
Anthial <faildude@live.se>
2022-12-19 14:24:04 +0100

    Fix inability to navigate away on permanent spinner when API calls fail.

--- src/frontend/views/spinnerView.tsx
+++ src/frontend/views/spinnerView.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 function Spinner() {
+  const navigate = useNavigate();
   return (
     <div className="w-full grow flex flex-col justify-center items-center">
+      <button className="self-center mb-3 bg-[#4D194D] hover:bg-[#251a33] font-bold" onClick={() => navigate(-1)}>BACK</button>
       <img src="/spinner.svg" alt="Loading" width="128" height="128" />
     </div>
   );
